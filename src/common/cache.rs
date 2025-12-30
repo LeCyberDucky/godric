c3b26ec39ffad0b65b0b2f6c99acd9a08fcccf09
Andy <19520383+LeCyberDucky@users.noreply.github.com>
2025-12-30 12:01:21 +0100

    Validate cache location

--- src/common/cache.rs
+++ src/common/cache.rs
@@ -1,12 +1,31 @@
 #[derive(Clone, Debug)]
 pub struct Config {
-    pub path: std::path::PathBuf
+    path: std::path::PathBuf,
+    valid: bool,
+}
+
+impl Config {
+    pub fn new(path: std::path::PathBuf) -> Self {
+        let valid = path.is_dir()
+            && std::fs::metadata(&path).is_ok_and(|meta| !meta.permissions().readonly());
+
+        Self {
+            path,
+            valid,
+        }
+    }
+
+    pub fn valid(&self) -> bool {
+        self.valid
+    }
+
+    pub fn path(&self) -> &std::path::PathBuf {
+        &self.path
+    }
 }
 
 impl Default for Config {
     fn default() -> Self {
-        Self {
-            path: "./Cache".into()
-        }
+        Self::new("./Cache".into())
     }
 }
--- src/scene/launch.rs
+++ src/scene/launch.rs
@@ -1,4 +1,4 @@
-use iced::Task;
+use iced::{Task, Theme, widget::text_input::default};
 use strum::IntoEnumIterator;
 
 use crate::{
@@ -128,7 +128,9 @@ impl Launch {
             Message::BrowserHeadlessToggle(headless) => self.browser_headless = headless,
             Message::ModeSelected(mode) => self.mode = mode,
             Message::BackendConnected => println!("Backend connected!"),
-            Message::CachePathInput(path) => self.cache.path = path.into(),
+            Message::CachePathInput(path) => {
+                self.cache = cache::Config::new(path.into())
+            }
         };
 
         (state.unwrap_or(self.into()), output, Task::none())
@@ -186,7 +188,14 @@ impl Launch {
             let cache_path = {
                 let title = iced::widget::text("Cache location");
                 let input =
-                    iced::widget::TextInput::new("./Cache", &self.cache.path.to_string_lossy())
+                    iced::widget::TextInput::new("./Cache", &self.cache.path().to_string_lossy())
+                        .style(|theme: &Theme, status| {
+                            let mut style = iced::widget::text_input::default(theme, status);
+                            if !self.cache.valid() {
+                                style.border.color = theme.palette().danger;
+                            }
+                            style
+                        })
                         .on_input(Message::CachePathInput)
                         .padding(10);
                 iced::widget::column!(title, input)
@@ -218,7 +227,7 @@ impl Launch {
         let launch_button = iced::widget::Button::new(
             iced::widget::Container::new("Launch!").center_x(iced::Length::Fill),
         )
-        .on_press(Message::LaunchAttempt)
+        .on_press_maybe(self.cache.valid().then_some(Message::LaunchAttempt))
         .width(iced::Length::Fill);
 
         let launch_prompt = iced::widget::column!(mode_selection, launch_button)
